fb92a4f0d9a12452fad38cf9732a99dd8dafdba0
Learning <learning@Ashleys-MacBook-Air.local>
2025-05-21 16:32:49 -0400

    first exercise

---
+++ data_types/practice.py
@@ -0,0 +1,13 @@
+my_tuple = (
+    'margot', 
+    'ren', 
+    'oswin', 
+    'zuzu', 
+    'weasel',
+)
+
+my_dictionary = {
+    'margot':   'chichi',
+    'oswin':    'whippet',
+    'zuzu':     'weasel',
+}
